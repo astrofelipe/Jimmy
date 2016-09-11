804865790ce5889eea0be01e3dfc7c1711dea1c0
astrofelipe <felipe.irh@gmail.com>
2016-09-11 19:14:11 -0300

    LPV

--- LPV.py
+++ LPV.py
@@ -1,5 +1,6 @@
 import glob
 import numpy as np
+from astropy.stats import mad_std
 
 #Test de chi2
 def redchi2(y, m, s, dof=0):
@@ -7,13 +8,32 @@ def redchi2(y, m, s, dof=0):
     nu   = len(data) - 1 - dof
     return chi2/nu
 
+#Abre curva de luz
+def openlc(f):
+    data = np.genfromtxt(f, usecols=(0,1,2))
+    data = data[data[:,1] < 98]
+
+    #Sigma clip
+    med = np.median(data[:,1])
+    std = mad_std(data[:,1])
+    res = data[:,1] - med
+    cli = np.abs(res) < 3*std
+
+    #Variables
+    t, m, e = np.transpose(data[cli])
+    return t, m, e
+
+#Abre y hace Test
+def combo(f):
+    t, m, e = openlc(f)
+
+    m1 = np.mean(m)
+    m2 = np.polyval(np.polyfit(t, m, 2), t)
+
 varcandi = glob.glob('results/*varcandi.txt')
 
 for v in varcandi:
     fns = np.genfromtxt(v, dtype='string')
 
     for f in fns:
-        t, f, e = np.genfromtxt(f, usecols=(0,1,2), unpack=True)
-
-        #m1 = np.mean
-        print f.max(), f.min()
+        combo(f)
