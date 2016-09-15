d7b576e4d416b61d946328f7d72cd8df1bc9eaac
Felipe Rojas <firojas@uc.cl>
2016-09-15 10:54:49 -0400

    update

--- plot_LPVs.py
+++ plot_LPVs.py
@@ -1,10 +1,24 @@
+import matplotlib
+matplotlib.use('Agg')
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
 from astropy.stats import mad_std
+from utils import isAlias
 
 name = np.genfromtxt(sys.argv[1], usecols=(0,), dtype='string')
-chi1, chi3 = np.genfromtxt(sys.argv[1], usecols=(1,2))
+chi1, chi3, p, fap = np.genfromtxt(sys.argv[1], usecols=(1,2,3,4), unpack=True)
+
+chi = chi3/chi1
+ma  = chi < 0.4
+
+name = name[ma]
+chi  = chi[ma]
+p    = p[ma]
+fap  = fap[ma]
+
+alias = isAlias(p)
+acols = ['#017d3f' if not al  else '#a4142a' for al in alias]
 
 #Faltan los matches aca
 
@@ -17,7 +31,7 @@ aa      = np.ravel(ax)
 for i in range(len(name)):
     a = aa[i]
     l = name[i]
-    t,m,e = np.genfomtxt(l, unpack=True, usecols=(0,1,2))
+    t,m,e = np.genfromtxt(l, unpack=True, usecols=(0,1,2))
     ph    = t / p[i] % 1.0
 
     mag_ma = m < 90
@@ -31,6 +45,9 @@ for i in range(len(name)):
 
     a.plot(ph[ma], m[ma], 'o', color='#0083cb', ms=3)
     a.plot(ph[ma]+1, m[ma], 'o', color='#0083cb', ms=3)
+
+    a.text(0.95, 0.1, 'P = %f' % p[i], ha='right', va='center', transform=a.transAxes, color=acols[i])
+
 
     lims  = a.get_ylim()
     ticks = np.arange(12, 21)
